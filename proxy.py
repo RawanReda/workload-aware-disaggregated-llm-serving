faf398e0242892135e78758e75c65d3d5aea4d67
Rawan Reda <rawan.reda.academia@gmail.com>
2026-07-27 00:16:38 +0300

    Implement create_completions method

--- proxy.py
+++ proxy.py
@@ -3,6 +3,7 @@ from contextlib import asynccontextmanager
 
 import httpx
 from fastapi import FastAPI, Request
+from fastapi.responses import StreamingResponse
 import uvicorn
 
 
@@ -12,7 +13,7 @@ def parse_args():
     parser.add_argument("--port", type=int, default=8000, help="Port for the proxy server")
     parser.add_argument("--prefiller-port", "-p", type=int, default=8100, help="Port for the prefill server")
     parser.add_argument("--decoder-port", "-d", type=int, default=8200, help="Port for the decoder server")
-    parser.add_argument("--model-name", "-m", type=str, default="<TO_DO>", help="Model name for the prefill and decoder servers")
+    parser.add_argument("--model-name", "-m", type=str, default="Qwen/Qwen2.5-1.5B-Instruct", help="Model name for the prefill and decoder servers")
     return parser.parse_args()
 
 
@@ -34,10 +35,35 @@ app = FastAPI(lifespan=lifespan)
 
 
 @app.post("/v1/completions")
-async def create_completions():
-    # This is a placeholder for the actual implementation of the /v1/completions endpoint.
-    # You would typically forward the request to the prefill and decoder servers here.
-    return {"message": "This endpoint will handle completions."} #streaming response 
+async def create_completions(request: Request):
+
+    try:
+        req_data = await request.json()
+
+        prefill_prepare_request = req_data.copy()
+
+        # Set max_tokens to 1 is a signal to the prefill service that we only want the prefill step and and the kv cache.
+        # The decoder service will handle the rest of the token generation.
+        prefill_prepare_request["max_tokens"] = 1
+
+        prefill_response = await app.state.prefill_client.post("/completions", json=prefill_prepare_request)
+        prefill_response.raise_for_status()
+ 
+
+        async def async_stream_generator():
+            async with app.state.decode_client.stream("POST", "/completions", json=req_data) as stream_response:
+                stream_response.raise_for_status()
+                async for chunk in stream_response.aiter_bytes():
+                    yield chunk
+
+        return StreamingResponse(
+            async_stream_generator(),
+            media_type="text/event-stream",
+        )
+
+    except Exception as e:
+        return {"error": str(e)}
+
 
 
 if __name__ == "__main__":
